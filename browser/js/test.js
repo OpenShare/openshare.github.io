bccc48f8b3eeec1fe4886e0be2e7eb8636826abc
Adam Chambers <adammchambers@gmail.com>
2016-07-22 13:04:01 -0400

    Fix all linting and ES6ify all the things

--- browser/js/test.js
+++ browser/js/test.js
@@ -1,7 +1,7 @@
-var OpenShare = {
+const OpenShare = {
 	share: require('open-share/share'),
 	count: require('open-share/count'),
-	analytics: require('open-share/analytics')
+	analytics: require('open-share/analytics'),
 };
 
 // OpenShare.analytics('tagManager', function () {
@@ -16,16 +16,16 @@ var OpenShare = {
 //   console.log('google analytics loaded');
 // });
 
-var dynamicNodeData = {
-	'url': 'http://www.digitalsurgeons.com',
-	'via': 'digitalsurgeons',
-	'text': 'Forward Obsessed',
-	'hashtags': 'forwardobsessed',
-	'button': 'Open Share Watcher!'
+const dynamicNodeData = {
+	url: 'http://www.digitalsurgeons.com',
+	via: 'digitalsurgeons',
+	text: 'Forward Obsessed',
+	hashtags: 'forwardobsessed',
+	button: 'Open Share Watcher!',
 };
 
 function createOpenShareNode(data) {
-	var openShare = document.createElement('a');
+	const openShare = document.createElement('a');
 
 	openShare.classList.add('open-share-link', 'twitter');
 	openShare.setAttribute('data-open-share', 'twitter');
@@ -33,9 +33,9 @@ function createOpenShareNode(data) {
 	openShare.setAttribute('data-open-share-via', data.via);
 	openShare.setAttribute('data-open-share-text', data.text);
 	openShare.setAttribute('data-open-share-hashtags', data.hashtags);
-	openShare.innerHTML = '<span class="fa fa-twitter"></span>' + data.button;
+	openShare.innerHTML = `<span class="fa fa-twitter"></span>${data.button}`;
 
-	var node = new OpenShare.share({
+	OpenShare.share({
 		type: 'twitter',
 		url: 'http://www.digitalsurgeons.com',
 		via: 'digitalsurgeons',
@@ -43,14 +43,14 @@ function createOpenShareNode(data) {
 		appendTo: document.querySelector('.open-share-watch'),
 		innerHTML: 'Created via OpenShareAPI',
 		element: 'div',
-		classes: ['wow', 'such', 'classes']
+		classes: ['wow', 'such', 'classes'],
 	});
 
 	return openShare;
 }
 
 function addNode() {
-	var data = dynamicNodeData;
+	const data = dynamicNodeData;
 	document.querySelector('.open-share-watch')
 		.appendChild(createOpenShareNode(data));
 }
@@ -58,41 +58,40 @@ function addNode() {
 window.addNode = addNode;
 
 function addNodeWithCount() {
-	var data = dynamicNodeData;
-
-	new OpenShare.count({
+	OpenShare.count({
 		type: 'facebook',
-		url: 'https://www.digitalsurgeons.com/'
-	}, function (node) {
-		var os = new OpenShare.share({
-		  type: 'twitter',
-		  url: 'http://www.digitalsurgeons.com',
-		  via: 'digitalsurgeons',
-		  hashtags: 'forwardobsessed',
-		  innerHTML: 'Created via OpenShareAPI',
-		  element: 'div',
-		  classes: ['wow', 'such', 'classes']
-	  });
-		document.querySelector('.create-node.w-count')
-		  .appendChild(os);
-		  os.appendChild(node);
+		url: 'https://www.digitalsurgeons.com/',
+	}, (node) => {
+		const os = new OpenShare.share({
+			type: 'twitter',
+			url: 'http://www.digitalsurgeons.com',
+			via: 'digitalsurgeons',
+			hashtags: 'forwardobsessed',
+			innerHTML: 'Created via OpenShareAPI',
+			element: 'div',
+			classes: ['wow', 'such', 'classes'],
+		});
+
+		document.querySelector('.create-node.w-count').appendChild(os);
+		os.appendChild(node);
 	});
 }
 
 window.addNodeWithCount = addNodeWithCount;
 
 function createCountNode() {
- 	var container = document.querySelector('.create-node.count-nodes');
-	var type = container.querySelector('input.count-type').value;
-	var url = container.querySelector('input.count-url').value;
+	const container = document.querySelector('.create-node.count-nodes');
+	const type = container.querySelector('input.count-type').value;
+	const url = container.querySelector('input.count-url').value;
 
-	new OpenShare.count({
-		type: type,
-		url: url,
+	OpenShare.count({
+		type,
+		url,
 		appendTo: container,
-		classes: ['test']
-	}, function (node) {
-		node.style.position = 'relative';
+		classes: ['test'],
+	}, (node) => {
+		const n = node;
+		n.style.position = 'relative';
 	});
 
 
@@ -104,88 +103,86 @@ window.createCountNode = createCountNode;
 
 // test JS OpenShare API with dashes
 
-new OpenShare.share({
+OpenShare.share({
 	type: 'googleMaps',
 	center: '40.765819,-73.975866',
 	view: 'traffic',
 	zoom: 14,
 	appendTo: document.body,
-	innerHTML: 'Maps'
+	innerHTML: 'Maps',
 });
 
-new OpenShare.share({
+OpenShare.share({
 	type: 'twitter-follow',
 	screenName: 'digitalsurgeons',
 	userId: '18189130',
 	appendTo: document.body,
-	innerHTML: 'Follow Test'
+	innerHTML: 'Follow Test',
 });
 
 // test PayPal
-new OpenShare.share({
+OpenShare.share({
 	type: 'paypal',
 	buttonId: '2P3RJYEFL7Z62',
 	sandbox: true,
 	appendTo: document.body,
-	innerHTML: 'PayPal Test'
+	innerHTML: 'PayPal Test',
 });
 
 // bind to count loaded event
-document.addEventListener('OpenShare.count-loaded', function() {
+document.addEventListener('OpenShare.count-loaded', () => {
 	console.log('OpenShare (count) loaded');
 });
 
 // bind to share loaded event
-document.addEventListener('OpenShare.share-loaded', function() {
+document.addEventListener('OpenShare.share-loaded', () => {
 	console.log('OpenShare (share) loaded');
 
 	// bind to shared event on each individual node
-	[].forEach.call(document.querySelectorAll('[data-open-share]'), function(node) {
-		node.addEventListener('OpenShare.shared', function(e) {
+	[].forEach.call(document.querySelectorAll('[data-open-share]'), (node) => {
+		node.addEventListener('OpenShare.shared', (e) => {
 			console.log('Open Share Shared', e);
 		});
 	});
 
-	var examples = {
-		twitter: new OpenShare.share({
-			type: 'twitter',
-			bindClick: true,
-			url: 'http://digitalsurgeons.com',
-			via: 'digitalsurgeons',
-			text: 'Digital Surgeons',
-			hashtags: 'forwardobsessed'
-		}, document.querySelector('[data-api-example="twitter"]')),
+	OpenShare.share({
+		type: 'twitter',
+		bindClick: true,
+		url: 'http://digitalsurgeons.com',
+		via: 'digitalsurgeons',
+		text: 'Digital Surgeons',
+		hashtags: 'forwardobsessed',
+	}, document.querySelector('[data-api-example="twitter"]'));
 
-		facebook: new OpenShare.share({
-			type: 'facebook',
-			bindClick: true,
-			link: 'http://digitalsurgeons.com',
-			picture: 'http://www.digitalsurgeons.com/img/about/bg_office_team.jpg',
-			caption: 'Digital Surgeons',
-			description: 'forwardobsessed'
-		}, document.querySelector('[data-api-example="facebook"]')),
+	OpenShare.share({
+		type: 'facebook',
+		bindClick: true,
+		link: 'http://digitalsurgeons.com',
+		picture: 'http://www.digitalsurgeons.com/img/about/bg_office_team.jpg',
+		caption: 'Digital Surgeons',
+		description: 'forwardobsessed',
+	}, document.querySelector('[data-api-example="facebook"]'));
 
-		pinterest: new OpenShare.share({
-			type: 'pinterest',
-			bindClick: true,
-			url: 'http://digitalsurgeons.com',
-			media: 'http://www.digitalsurgeons.com/img/about/bg_office_team.jpg',
-			description: 'Digital Surgeons',
-			appendTo: document.body
-		}, document.querySelector('[data-api-example="pinterest"]')),
+	OpenShare.share({
+		type: 'pinterest',
+		bindClick: true,
+		url: 'http://digitalsurgeons.com',
+		media: 'http://www.digitalsurgeons.com/img/about/bg_office_team.jpg',
+		description: 'Digital Surgeons',
+		appendTo: document.body,
+	}, document.querySelector('[data-api-example="pinterest"]'));
 
-		email: new OpenShare.share({
-			type: 'email',
-			bindClick: true,
-			to: 'techroom@digitalsurgeons.com',
-			subject: 'Digital Surgeons',
-			body: 'Forward Obsessed'
-		}, document.querySelector('[data-api-example="email"]'))
-	};
+	OpenShare.share({
+		type: 'email',
+		bindClick: true,
+		to: 'techroom@digitalsurgeons.com',
+		subject: 'Digital Surgeons',
+		body: 'Forward Obsessed',
+	}, document.querySelector('[data-api-example="email"]'));
 });
 
 // Example of listening for counted events on individual urls or arrays of urls
-var urls = [
+const urls = [
 	'facebook',
 	'google',
 	'linkedin',
@@ -195,19 +192,21 @@ var urls = [
 		'google',
 		'linkedin',
 		'reddit',
-		'pinterest'
-	]
+		'pinterest',
+	],
 ];
 
-urls.forEach(function(url) {
-	if (Array.isArray(url)) {
-		url = url.join(',');
+urls.forEach((url) => {
+	let u = url;
+	if (Array.isArray(u)) {
+		u = u.join(',');
 	}
-	var countNode = document.querySelectorAll('[data-open-share-count="' + url + '"]');
 
-	[].forEach.call(countNode, function(node) {
-		node.addEventListener('OpenShare.counted-' + url, function() {
-			var counts = node.innerHTML;
+	const countNode = document.querySelectorAll(`[data-open-share-count="${url}"]`);
+
+	[].forEach.call(countNode, (node) => {
+		node.addEventListener(`OpenShare.counted-${url}`, () => {
+			const counts = node.innerHTML;
 			if (counts) console.log(url, 'shares: ', counts);
 		});
 	});
